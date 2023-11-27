78e67d449fd030b00b15725c0d499b67773bda7e
GET Academy <terje@kolderup.net>
2023-11-27 09:18:36 +0100

    Initial commit

---
+++ IntroEncapsulationPlayerInHouse/House.cs
@@ -0,0 +1,16 @@
+﻿namespace IntroEncapsulationPlayerInHouse
+{
+    internal class House
+    {
+
+        public House()
+        {
+            
+        }
+
+        public void MoveTo()
+        {
+
+        }
+    }
+}
